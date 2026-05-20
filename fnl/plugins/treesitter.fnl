42570e768e7c69a0018d786bd286a71af6450562
lowerc4s3 <kkirill03@gmail.com>
2026-05-20 15:16:36 +0300

    feat: treesitter (ts-enable)

---
+++ fnl/plugins/treesitter.fnl
@@ -0,0 +1,3 @@
+{1 :VonHeikemen/ts-enable.nvim
+ :version :v2.x
+ :init #(set vim.g.ts_enable {:auto_init true :highlights true :folds true})}
---
+++ lua/plugins/treesitter.lua
@@ -0,0 +1,6 @@
+-- [nfnl] fnl/plugins/treesitter.fnl
+local function _1_()
+  vim.g.ts_enable = {auto_init = true, highlights = true, folds = true}
+  return nil
+end
+return {"VonHeikemen/ts-enable.nvim", version = "v2.x", init = _1_}
