9ef19eebc22b535d95ece2d48807d8aa6a09c779
lowerc4s3 <kkirill03@gmail.com>
2026-05-22 01:26:21 +0300

    feat: minibuffer which-key intergration (experimental)

--- fnl/plugins/which-key.fnl
+++ fnl/plugins/which-key.fnl
@@ -1,7 +1,14 @@
-(import-macros {: gh : reqfn} :lib.macros)
+(import-macros {: gh : setup} :lib.macros)
 
 {; keymap cheatsheet
  :src (gh :folke/which-key.nvim)
+ :dependencies {:src (gh :simifalaye/minibuffer.nvim)}
+ :config (fn [_ opts]
+           (setup :which-key opts)
+           (let [minibuf-wk (require :minibuffer.integrations.which-key)
+                 wk-view (require :which-key.view)]
+             (set wk-view.show minibuf-wk.show)
+             (set wk-view.hide minibuf-wk.hide)))
  :opts {:plugins {:marks false
                   :registers false
                   :presets {:operators false
@@ -12,6 +19,7 @@
         :show_help false
         :delay #(or (and $1.plugin 0) 200)
         :filter #(and $1.desc (not= 1 ($1.desc:find :langmapped 1 true)))
+        :win {:no_overlap false}
         :icons {:mappings false
                 :separator ":"
                 :keys {:up :<up>
--- lua/plugins/which-key.lua
+++ lua/plugins/which-key.lua
@@ -1,8 +1,16 @@
 -- [nfnl] fnl/plugins/which-key.fnl
-local function _1_(_241)
-  return ((_241.plugin and 0) or 200)
+local function _1_(_, opts)
+  require("which-key").setup(opts)
+  local minibuf_wk = require("minibuffer.integrations.which-key")
+  local wk_view = require("which-key.view")
+  wk_view.show = minibuf_wk.show
+  wk_view.hide = minibuf_wk.hide
+  return nil
 end
 local function _2_(_241)
+  return ((_241.plugin and 0) or 200)
+end
+local function _3_(_241)
   return (_241.desc and (1 ~= _241.desc:find("langmapped", 1, true)))
 end
-return {src = "https://github.com/folke/which-key.nvim", opts = {plugins = {presets = {motions = false, nav = false, operators = false, text_objects = false, windows = false}, marks = false, registers = false}, delay = _1_, filter = _2_, icons = {separator = ":", keys = {up = "<up>", down = "<down>", left = "<left>", right = "<right>", C = "C-", M = "M-", D = "D-", S = "S-", CR = "RET", NL = "RET", BS = "DEL", Space = "SPC", Tab = "TAB", Esc = "ESC"}, mappings = false}, spec = {{"<Leader>f", group = "find"}}, show_help = false}}
+return {src = "https://github.com/folke/which-key.nvim", dependencies = {src = "https://github.com/simifalaye/minibuffer.nvim"}, config = _1_, opts = {plugins = {presets = {motions = false, nav = false, operators = false, text_objects = false, windows = false}, marks = false, registers = false}, delay = _2_, filter = _3_, win = {no_overlap = false}, icons = {separator = ":", keys = {up = "<up>", down = "<down>", left = "<left>", right = "<right>", C = "C-", M = "M-", D = "D-", S = "S-", CR = "RET", NL = "RET", BS = "DEL", Space = "SPC", Tab = "TAB", Esc = "ESC"}, mappings = false}, spec = {{"<Leader>f", group = "find"}}, show_help = false}}
