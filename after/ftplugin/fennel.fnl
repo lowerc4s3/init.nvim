0ccdf467f22bc8f0b8b770c35ff6971a4fd54652
lowerc4s3 <kkirill03@gmail.com>
2026-05-26 12:38:54 +0300

    feat: fennel ftplugin

---
+++ after/ftplugin/fennel.fnl
@@ -0,0 +1,6 @@
+(local opt vim.opt)
+
+(let [width 2]
+  (set opt.shiftwidth width)
+  (set opt.tabstop width)
+  (set opt.softtabstop width))
---
+++ after/ftplugin/fennel.lua
@@ -0,0 +1,7 @@
+-- [nfnl] after/ftplugin/fennel.fnl
+local opt = vim.opt
+local width = 2
+opt.shiftwidth = width
+opt.tabstop = width
+opt.softtabstop = width
+return nil
