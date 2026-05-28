b17daea4e4d06097fc8140eeaf618a29d1e6ed17
lowerc4s3 <kkirill03@gmail.com>
2026-05-28 20:06:54 +0300

    feat: neogit configuration

--- fnl/plugins/neogit.fnl
+++ fnl/plugins/neogit.fnl
@@ -3,8 +3,12 @@
 ;; magit flavored git client
 {:src (gh :NeogitOrg/neogit)
  :dependencies {:src (gh :dlyongemallo/diffview.nvim)}
- :lazy false
- :config #(! (require :which-key) :add [{1 :<Leader>g :group :git}])
+ :cmd :Neogit
+ :opts {:disable_hint true
+        :graph_style :unicode}
+ :config (fn [_ opts]
+           (! (require :neogit) :setup opts)
+           (! (require :which-key) :add [{1 :<Leader>g :group :git}]))
  :keys [(specmap :n :<Leader>gg (cmd :Neogit) {:desc "open neogit tab"})
         (specmap :n :<Leader>gl (cmd "Neogit log") {:desc "view log"})
         (specmap :n :<Leader>gp (cmd "Neogit pull") {:desc :pull})
--- lua/plugins/neogit.lua
+++ lua/plugins/neogit.lua
@@ -1,5 +1,6 @@
 -- [nfnl] fnl/plugins/neogit.fnl
-local function _1_()
+local function _1_(_, opts)
+  require("neogit").setup(opts)
   return require("which-key").add({{"<Leader>g", group = "git"}})
 end
-return {src = "https://github.com/NeogitOrg/neogit", dependencies = {src = "https://github.com/dlyongemallo/diffview.nvim"}, config = _1_, keys = {{"<Leader>gg", "<Cmd>Neogit<CR>", desc = "open neogit tab", modes = "n", silent = true}, {"<Leader>gl", "<Cmd>Neogit log<CR>", desc = "view log", modes = "n", silent = true}, {"<Leader>gp", "<Cmd>Neogit pull<CR>", desc = "pull", modes = "n", silent = true}, {"<Leader>gP", "<Cmd>Neogit push<CR>", desc = "push", modes = "n", silent = true}}, lazy = false}
+return {src = "https://github.com/NeogitOrg/neogit", dependencies = {src = "https://github.com/dlyongemallo/diffview.nvim"}, cmd = "Neogit", opts = {disable_hint = true, graph_style = "unicode"}, config = _1_, keys = {{"<Leader>gg", "<Cmd>Neogit<CR>", desc = "open neogit tab", modes = "n", silent = true}, {"<Leader>gl", "<Cmd>Neogit log<CR>", desc = "view log", modes = "n", silent = true}, {"<Leader>gp", "<Cmd>Neogit pull<CR>", desc = "pull", modes = "n", silent = true}, {"<Leader>gP", "<Cmd>Neogit push<CR>", desc = "push", modes = "n", silent = true}}}
