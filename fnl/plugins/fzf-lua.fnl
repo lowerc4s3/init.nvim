db78882612853ceb81ddff94a906c6faf144fe11
lowerc4s3 <kkirill03@gmail.com>
2026-05-31 16:48:40 +0300

    feat: don't use icons in fzf-lua, add keymaps
    icons noticeably slow down files picker

--- fnl/plugins/fzf-lua.fnl
+++ fnl/plugins/fzf-lua.fnl
@@ -1,29 +1,35 @@
 (import-macros {: gh : specmap : cmd : !} :lib.macros)
 
 (macro fzf [command]
-  `(cmd ,(.. :FzfLua " " command)))
+  (assert-compile (sym? command) "expected symbol for a command")
+  `(cmd ,(.. :FzfLua " " (view command))))
 
 ;; fzf-based fuzzy picker
 {:src (gh :ibhagwan/fzf-lua)
  :dependencies :nvim-mini/mini.icons
  :lazy false
- :opts {1 [:ivy :borderless :hide]
+ :opts {1 [:ivy :borderless :hide] ; enable presets
         :fzf_colors true
         :ui_select true
-        :defaults {:file_icons true}}
+        :keymap {:fzf {1 true ; inherit default keymaps
+                       :ctrl-A :toggle-all}}
+        :actions {:files {1 true
+                          :ctrl-h #(_G.FzfLua.actions.toggle_hidden $...)
+                          :ctrl-Q #(_G.FzfLua.actions.file_sel_to_qf $...)
+                          :alt-Q #(_G.FzfLua.actions.file_sel_to_ll $...)}}}
  :config (fn [_ opts]
            (! (require :fzf-lua) :setup opts)
            (! (require :which-key) :add [{1 :<Leader>s :group :search}
                                          {1 :<Leader>h :group :help}]))
- :keys [(specmap :n :<Leader><Leader> (fzf :files) {:desc "open cwd file"})
-        (specmap :n "<Leader>," (fzf :buffers) {:desc "switch buffer"})
-        (specmap :n "<Leader>'" (fzf :resume) {:desc "resume last search"})
-        (specmap :n :<Leader>sf (fzf :files) {:desc "cwd file"})
-        (specmap :n :<Leader>sb (fzf :buffers) {:desc :buffer})
-        (specmap :n :<Leader>sw (fzf :live_grep) {:desc "live grep"})
-        (specmap :n :<Leader>sr (fzf :history) {:desc "recent file"})
-        (specmap :n :<Leader>ss (fzf :builtin) {:desc "select picker"})
-        (specmap :n :<Leader>hh (fzf :helptags) {:desc :helptags})
-        (specmap :n :<Leader>hk (fzf :keymaps) {:desc :keymaps})
-        (specmap :n :<Leader>ho (fzf :nvim_options) {:desc :options})
-        (specmap :n :<Leader>hH (fzf :highlights) {:desc :highlights})]}
+ :keys [(specmap :n :<Leader><Leader> (fzf files) {:desc "open cwd file"})
+        (specmap :n "<Leader>," (fzf buffers) {:desc "switch buffer"})
+        (specmap :n "<Leader>'" (fzf resume) {:desc "resume last search"})
+        (specmap :n :<Leader>sf (fzf files) {:desc "cwd file"})
+        (specmap :n :<Leader>sb (fzf buffers) {:desc :buffer})
+        (specmap :n :<Leader>sw (fzf live_grep) {:desc "live grep"})
+        (specmap :n :<Leader>sr (fzf history) {:desc "recent file"})
+        (specmap :n :<Leader>ss (fzf builtin) {:desc "select picker"})
+        (specmap :n :<Leader>hh (fzf helptags) {:desc :helptags})
+        (specmap :n :<Leader>hk (fzf keymaps) {:desc :keymaps})
+        (specmap :n :<Leader>ho (fzf nvim_options) {:desc :options})
+        (specmap :n :<Leader>hH (fzf highlights) {:desc :highlights})]}
--- lua/plugins/fzf-lua.lua
+++ lua/plugins/fzf-lua.lua
@@ -1,6 +1,15 @@
 -- [nfnl] fnl/plugins/fzf-lua.fnl
-local function _1_(_, opts)
+local function _1_(...)
+  return _G.FzfLua.actions.toggle_hidden(...)
+end
+local function _2_(...)
+  return _G.FzfLua.actions.file_sel_to_qf(...)
+end
+local function _3_(...)
+  return _G.FzfLua.actions.file_sel_to_ll(...)
+end
+local function _4_(_, opts)
   require("fzf-lua").setup(opts)
   return require("which-key").add({{"<Leader>s", group = "search"}, {"<Leader>h", group = "help"}})
 end
-return {src = "https://github.com/ibhagwan/fzf-lua", dependencies = "nvim-mini/mini.icons", opts = {{"ivy", "borderless", "hide"}, fzf_colors = true, ui_select = true, defaults = {file_icons = true}}, config = _1_, keys = {{"<Leader><Leader>", "<Cmd>FzfLua files<CR>", desc = "open cwd file", modes = "n", silent = true}, {"<Leader>,", "<Cmd>FzfLua buffers<CR>", desc = "switch buffer", modes = "n", silent = true}, {"<Leader>'", "<Cmd>FzfLua resume<CR>", desc = "resume last search", modes = "n", silent = true}, {"<Leader>sf", "<Cmd>FzfLua files<CR>", desc = "cwd file", modes = "n", silent = true}, {"<Leader>sb", "<Cmd>FzfLua buffers<CR>", desc = "buffer", modes = "n", silent = true}, {"<Leader>sw", "<Cmd>FzfLua live_grep<CR>", desc = "live grep", modes = "n", silent = true}, {"<Leader>sr", "<Cmd>FzfLua history<CR>", desc = "recent file", modes = "n", silent = true}, {"<Leader>ss", "<Cmd>FzfLua builtin<CR>", desc = "select picker", modes = "n", silent = true}, {"<Leader>hh", "<Cmd>FzfLua helptags<CR>", desc = "helptags", modes = "n", silent = true}, {"<Leader>hk", "<Cmd>FzfLua keymaps<CR>", desc = "keymaps", modes = "n", silent = true}, {"<Leader>ho", "<Cmd>FzfLua nvim_options<CR>", desc = "options", modes = "n", silent = true}, {"<Leader>hH", "<Cmd>FzfLua highlights<CR>", desc = "highlights", modes = "n", silent = true}}, lazy = false}
+return {src = "https://github.com/ibhagwan/fzf-lua", dependencies = "nvim-mini/mini.icons", opts = {{"ivy", "borderless", "hide"}, fzf_colors = true, ui_select = true, keymap = {fzf = {true, ["ctrl-A"] = "toggle-all"}}, actions = {files = {true, ["ctrl-h"] = _1_, ["ctrl-Q"] = _2_, ["alt-Q"] = _3_}}}, config = _4_, keys = {{"<Leader><Leader>", "<Cmd>FzfLua files<CR>", desc = "open cwd file", modes = "n", silent = true}, {"<Leader>,", "<Cmd>FzfLua buffers<CR>", desc = "switch buffer", modes = "n", silent = true}, {"<Leader>'", "<Cmd>FzfLua resume<CR>", desc = "resume last search", modes = "n", silent = true}, {"<Leader>sf", "<Cmd>FzfLua files<CR>", desc = "cwd file", modes = "n", silent = true}, {"<Leader>sb", "<Cmd>FzfLua buffers<CR>", desc = "buffer", modes = "n", silent = true}, {"<Leader>sw", "<Cmd>FzfLua live_grep<CR>", desc = "live grep", modes = "n", silent = true}, {"<Leader>sr", "<Cmd>FzfLua history<CR>", desc = "recent file", modes = "n", silent = true}, {"<Leader>ss", "<Cmd>FzfLua builtin<CR>", desc = "select picker", modes = "n", silent = true}, {"<Leader>hh", "<Cmd>FzfLua helptags<CR>", desc = "helptags", modes = "n", silent = true}, {"<Leader>hk", "<Cmd>FzfLua keymaps<CR>", desc = "keymaps", modes = "n", silent = true}, {"<Leader>ho", "<Cmd>FzfLua nvim_options<CR>", desc = "options", modes = "n", silent = true}, {"<Leader>hH", "<Cmd>FzfLua highlights<CR>", desc = "highlights", modes = "n", silent = true}}, lazy = false}
