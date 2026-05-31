4c00de6aab5cede62d59246b0df81d7f32b8d732
lowerc4s3 <kkirill03@gmail.com>
2026-05-31 21:29:22 +0300

    feat: enable mini-jump

--- fnl/plugins/mini-jump.fnl
+++ fnl/plugins/mini-jump.fnl
@@ -2,7 +2,6 @@
 
 ;; repeat f/F/t/T to search letters (clever-f style)
 {:src (gh :nvim-mini/mini.jump)
- :enabled false
  :config (fn []
            (! (require :mini.jump) :setup)
-           (vim.api.nvim_set_hl 0 :MiniJump {:link :Search}))}
+           (vim.api.nvim_set_hl 0 :MiniJump {:link :CurSearch}))}
--- lua/plugins/mini-jump.lua
+++ lua/plugins/mini-jump.lua
@@ -1,6 +1,6 @@
 -- [nfnl] fnl/plugins/mini-jump.fnl
 local function _1_()
   require("mini.jump").setup()
-  return vim.api.nvim_set_hl(0, "MiniJump", {link = "Search"})
+  return vim.api.nvim_set_hl(0, "MiniJump", {link = "CurSearch"})
 end
-return {src = "https://github.com/nvim-mini/mini.jump", config = _1_, enabled = false}
+return {src = "https://github.com/nvim-mini/mini.jump", config = _1_}
